eb7ba5a076ba764df694a60dd5e117b76c248071
Christopher Brown <cb@opscode.com>
2010-02-03 22:09:25 -0800

    new models for databag & databagitem

---
+++ lib/mixlib/authorization/models/databagitem.rb
@@ -0,0 +1,48 @@
+#
+# Author:: Christopher Brown <cb@opscode.com>
+#
+# Copyright 2009, Opscode, Inc.
+#
+# All rights reserved - do not redistribute
+#
+
+module Mixlib
+  module Authorization
+    module Models
+      class DataBagItem < CouchRest::ExtendedDocument
+        include CouchRest::Validation
+        include Mixlib::Authorization::AuthHelper
+        include Mixlib::Authorization::JoinHelper
+        include Mixlib::Authorization::ContainerHelper
+        
+        use_database Mixlib::Authorization::Config.default_database
+        
+        view_by :name
+        
+        property :name
+        property :orgname
+        
+        validates_present :name, :orgname
+        
+        auto_validate!
+
+        create_callback :after, :create_join
+        update_callback :after, :update_join
+        destroy_callback :before, :delete_join
+        
+        join_type Mixlib::Authorization::Models::JoinTypes::Object 
+        join_properties :requester_id
+        
+        def for_json
+          self.properties.inject({ }) do |result, prop|
+            pname = prop.name.to_sym
+            #BUGBUG - I hate stripping properties like this.  We should do it differently [cb]      
+            result[pname] = self.send(pname) unless pname == :requester_id
+            result
+          end
+        end
+        
+      end
+    end
+  end
+end

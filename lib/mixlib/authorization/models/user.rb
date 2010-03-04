6f051569b5384845193c965d6aa7734b42da6afa
Daniel DeLeo <dan@opscode.com>
2010-03-04 12:51:54 -0800

    log the exception to debug when uniqueness test fails

--- lib/mixlib/authorization/models/user.rb
+++ lib/mixlib/authorization/models/user.rb
@@ -63,6 +63,8 @@ module Mixlib
             return true if (how_many == 0) || (how_many == 1 && self.has_key?('_id'))
           rescue StandardError => se
             Mixlib::Authorization::Log.error "Failed to determine if username '#{self['username']}' is unique"
+            Mixlib::Authorization::Log.debug(se.inspect)
+            Mixlib::Authorization::Log.debug(se.backtrace.join("\n"))
           end
           [ false, "The name #{self[:username]} is not unique!" ]
         end

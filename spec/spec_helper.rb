1c04f469173555e6c48df02b1cb6cda3f0d19fda
Daniel DeLeo <dan@opscode.com>
2010-07-26 16:44:46 -0700

    make RequestAuthentication a class, and use one obj per auth req

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'pp'
 require 'rubygems'
+
+gem "rest-client", ">= 1.0.3", "<= 1.0.5"
+
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/../lib/")
 require "mixlib/authorization"
 require 'mixlib/authorization/auth_helper'

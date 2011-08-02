141355199e94662ecef44ad1d1db6c57fec4951e
Daniel DeLeo <dan@opscode.com>
2011-08-02 16:52:07 -0700

    refactor stats collector into a mixin for controllers

--- lib/opscode/mappers/base.rb
+++ lib/opscode/mappers/base.rb
@@ -1,41 +1,7 @@
 require 'sequel'
 require 'yajl'
 require 'uuidtools'
-require 'statsd/client'
 
 # UTC4Life plz:
 Sequel.default_timezone = :utc
-
-module Opscode
-  module Mappers
-    module MapperStats
-
-      attr_reader :total_db_calls
-      attr_reader :total_db_time
-
-      def setup_request
-        @total_db_calls = 0
-        @total_db_time = 0
-      end
-
-      def db_call
-        begin
-          increment("upstreamRequests.totalDatabaseCalls")
-          @total_db_calls += 1
-          start_time = Time.now
-          yield
-        ensure
-          end_time = Time.now
-          elapsed_ms = ((end_time - start_time) * 1000).round
-          timing("upstreamRequests.databaseCall", elapsed_ms)
-          @total_db_time += elapsed_ms
-        end
-      end
-
-    end
-  end
-
-end
-
-Statsd::Client.send(:include, Opscode::Mappers::MapperStats)
 

4489571eb9dadffc1383796f922f13924d349d24
Daniel DeLeo <dan@opscode.com>
2011-09-22 17:00:05 -0700

    Add tests for join doc and refactor it.

--- lib/mixlib/authorization/models/join_document.rb
+++ lib/mixlib/authorization/models/join_document.rb
@@ -13,48 +13,80 @@ module Mixlib
   module Authorization
     module Models
       class JoinDocument
+
+        def self.authz_resource_name
+          @authz_resource_name ||= name.split("::").last.downcase + "s"
+        end
+
+        def resource
+          self.class.authz_resource_name
+        end
+
         attr_reader :identity
-        attr_reader :resource
         attr_reader :base_url
         attr_reader :model_data
+        attr_reader :requester_id
+        attr_reader :id
 
         alias :join_data :model_data
 
-        #include Mixlib::Authorization::AuthHelper
 
+        BASE_HEADERS = {:accept         => "application/json".freeze,
+                        :content_type   => "application/json".freeze,
+                        "X-Ops-User-Id".freeze => 'front-end-service'.freeze }.freeze
+
+        X_OPS_REQUESTING_ACTOR_ID = "X-Ops-Requesting-Actor-Id".freeze
+        REQUESTER_ID = "requester_id".freeze
+
+        FSLASH = "/".freeze
+
+        ACL = "acl".freeze
+        ACE = "ace".freeze
+        ACTORS = "actors".freeze
+        GROUPS = "groups".freeze
+        OBJECT_ID = "object_id".freeze
+        ID = "id"
+
+        # Create a new Authz side model.
+        # === Arguments
+        # * base_url::: The base URL for the authorization service
+        # === Params +model_data+
+        # * 'requester_id' ::: (mandatory) the authz id of the actor making the request
+        # * 'object_id' ::: (omitted for create operations, required
+        #   otherwise). The authz id of the object.
         def initialize(base_url,model_data)
-          @model_data = model_data
-          @resource = self.class.name.split("::").last.downcase + "s"
           @base_url = base_url
+          @model_data = model_data
+
+          unless @requester_id = model_data[REQUESTER_ID]
+            raise ArgumentError, "Cannot create a #{self.class} without a requester_id"
+          end
+
+          @id = model_data[OBJECT_ID]
+        end
+
+        def headers
+          @headers ||= begin
+            h = BASE_HEADERS.dup
+            h[X_OPS_REQUESTING_ACTOR_ID] = requester_id
+            h
+          end
         end
 
         def save
-          url = [base_url,resource].join("/")
-          requester_id = model_data["requester_id"]
           Mixlib::Authorization::Log.debug "IN SAVE: model_data #{model_data.inspect}"
-          headers = {:accept => "application/json", :content_type => "application/json"}
-          headers["X-Ops-Requesting-Actor-Id"] = requester_id if requester_id
-          headers["X-Ops-User-Id"] = 'front-end-service'
-
-          Mixlib::Authorization::Log.debug "IN SAVE: url: #{url.inspect}, with payload: #{model_data.to_json}"
-          rest = RestClient::Resource.new(url,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
+          rest = resource_for()
           @identity = JSON.parse(rest.post(model_data.to_json))
+          @id = @identity[ID]
           Mixlib::Authorization::Log.debug "IN SAVE: response: #{@identity.inspect}"
           @identity
         end
 
         def fetch
           Mixlib::Authorization::Log.debug "IN FETCH: #{self.inspect}"
-          object_id = model_data["object_id"]
-          url = [base_url,resource,object_id].join("/")
-          requester_id = model_data["requester_id"]
-          Mixlib::Authorization::Log.debug "IN FETCH: #{url.inspect}"
 
-          headers = {:accept => "application/json", :content_type => "application/json"}
-          headers["X-Ops-Requesting-Actor-Id"] = requester_id if requester_id
-          headers["X-Ops-User-Id"] = 'front-end-service'
-          rest = RestClient::Resource.new(url,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
-          @identity  = JSON.parse(rest.get).merge({ "id"=>object_id })
+          rest = resource_for(id)
+          @identity  = JSON.parse(rest.get).merge({ "id"=>id })
           Mixlib::Authorization::Log.debug "IN FETCH: response #{@identity.inspect}"
           @identity
         end
@@ -65,86 +97,91 @@ module Mixlib
 
         def fetch_acl
           Mixlib::Authorization::Log.debug "IN FETCH ACL: #{self.inspect}"
-          object_id = model_data["object_id"]
-          url = [base_url,resource,object_id,"acl"].join("/")
-          requester_id = model_data["requester_id"]
-          Mixlib::Authorization::Log.debug "IN FETCH ACL: #{url}"
 
-          headers = {:accept => "application/json", :content_type => "application/json"}
-          headers["X-Ops-Requesting-Actor-Id"] = requester_id if requester_id
-          headers["X-Ops-User-Id"] = 'front-end-service'
-          rest = RestClient::Resource.new(url,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
+          rest = resource_for(id, ACL)
           @identity  = JSON.parse(rest.get)
 
           Mixlib::Authorization::Log.debug "FETCH ACL: #{@identity.inspect}"
           @identity
         end
 
-        def is_authorized?(actor, ace)
-          object_id = model_data["object_id"]
-          url = [base_url,resource,object_id,"acl",ace,"actors", actor].join("/")
-          url_dbg = [base_url,resource,object_id,"acl",ace,].join("/")
-          requester_id = model_data["requester_id"]
-          Mixlib::Authorization::Log.debug "IN IS_AUTHORIZED: #{self.inspect} \n\twith actor: #{actor}\n\tace: #{ace}\n\turl:#{url}"
-
-          headers = {:accept=>"application/json", :content_type=>"application/json"}
-          headers["X-Ops-Requesting-Actor-Id"] = requester_id if requester_id
-          headers["X-Ops-User-Id"] = 'front-end-service'
-
-          begin
-            rest = RestClient::Resource.new(url,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
-            JSON.parse(rest.get)
-          rescue RestClient::ResourceNotFound
-            false
-          end
-
+        def authorized?(actor, ace)
+          rest = resource_for(id, ACL, ace, ACTORS, actor.to_s)
+          JSON.parse(rest.get)
+        rescue RestClient::ResourceNotFound
+          false
         end
+
+        # back compat. FYI, is_foo? is javaism
+        alias :is_authorized? :authorized?
 
         #e.g. ace_name: 'delete', ace_data: {"actors"=>["signing_caller"], "groups"=>[]}
         def update_ace(ace_name, ace_data)
           Mixlib::Authorization::Log.debug "IN UPDATE ACE: #{self.inspect}, ace_data: #{ace_data.inspect}"
 
           # update actors and groups
-          begin
-            object_id = model_data["object_id"]
-
-            headers = {:accept=>:json, :content_type=>:json, "X-Ops-Requesting-Actor-Id" => model_data["requester_id"], "X-Ops-User-Id"=>'front-end-service'}
-
-            url_get_ace = [base_url,resource, object_id,"acl",ace_name].join("/")
-            rest = RestClient::Resource.new(url_get_ace,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
-            current_ace = JSON.parse(rest.get)
-            new_ace = Hash.new
-            ["actors", "groups"].each do |actor_type|
-              if ace_data.has_key?(actor_type)
-                to_delete = current_ace[actor_type] - ace_data[actor_type]
-                to_put    = ace_data[actor_type] - current_ace[actor_type]
-                new_ace[actor_type] = current_ace[actor_type] - to_delete + to_put
-              end
+          rest = resource_for(id, ACL, ace_name)
+          current_ace = JSON.parse(rest.get)
+          new_ace = Hash.new
+          [ACTORS, GROUPS].each do |actor_type|
+            if ace_data.has_key?(actor_type)
+              to_delete = current_ace[actor_type] - ace_data[actor_type]
+              to_put    = ace_data[actor_type] - current_ace[actor_type]
+              new_ace[actor_type] = current_ace[actor_type] - to_delete + to_put
             end
-            Mixlib::Authorization::Log.debug("IN UPDATE ACE: Current ace: #{current_ace.inspect}, Future ace: #{new_ace.inspect}")
-            target_url = [base_url,resource, object_id,"acl",ace_name].join("/")
-            rest = RestClient::Resource.new(target_url,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
-            resp = JSON.parse(rest.put(new_ace.to_json))
-            Mixlib::Authorization::Log.debug("IN UPDATE ACE: response #{resp.inspect}")
-            resp
-          rescue StandardError => se
-            Mixlib::Authorization::Log.error "Failed to update ace: #{se.message} " + se.backtrace.join(",\n")
-            raise
           end
+
+          Mixlib::Authorization::Log.debug("IN UPDATE ACE: Current ace: #{current_ace.inspect}, Future ace: #{new_ace.inspect}")
+
+          rest = resource_for(id, ACL, ace_name)
+          resp = JSON.parse(rest.put(new_ace.to_json))
+
+          Mixlib::Authorization::Log.debug("IN UPDATE ACE: response #{resp.inspect}")
+          resp
+        rescue => se
+          Mixlib::Authorization::Log.error "Failed to update ace: #{se.message} " + se.backtrace.join(",\n")
+          raise
         end
 
         def delete
-          object_id = model_data["object_id"]
-          url = [base_url,resource,object_id].join("/")
-
-          headers = {:accept=>"application/json", :content_type=>'application/json'}
-          headers["X-Ops-Requesting-Actor-Id"] = requester_id if requester_id
-          headers["X-Ops-User-Id"] = 'front-end-service'
-          rest = RestClient::Resource.new(target_url,:headers=>headers, :timeout=>1800, :open_timeout=>1800)
+          rest = resource_for(id)
           resp = JSON.parse(rest.delete)
 
           @identity = resp
           true
+        end
+
+        # Merges the ACL of the given +container+ on to this object's ACL.
+        # +container+ should be the *AuthZ side* container object, not the user-side.
+        def apply_parent_acl(container)
+          container_acl_data = container.fetch_acl
+          container_acl = Acl.new(container_acl_data)
+          self_acl = Acl.new(fetch_acl)
+          self_acl.merge!(container_acl)
+          self_acl.aces.each {  |ace_name,ace| update_ace(ace_name, ace.ace) }
+        end
+
+        # Adds the actor given by +actor_id+ (an authz side id) to the
+        # specified ace for this object.
+        # Ex:
+        #   grant_permission_to_actor("read", client.authz_id)
+        #
+        def grant_permission_to_actor(ace_type, actor_id)
+          self_acl = Acl.new(fetch_acl)
+          target_ace = self_acl.aces[ace_type]
+          target_ace.add_actor(actor_id)
+          update_ace(ace_type, target_ace.ace)
+        end
+
+        # Create a RestClient::Resource for the given path components. See also: #url_for
+        def resource_for(*paths)
+          RestClient::Resource.new(url_for(*paths),:headers=>headers, :timeout=>10, :open_timeout=>5)
+        end
+
+        # Generate the URL for the given path components. If no components are
+        # given, it returns the base URL for the resource type (e.g., http://authz:2345/clients)
+        def url_for(*paths)
+          paths.inject("#{base_url}/#{resource}") {|url, component| url << FSLASH << component}
         end
 
       end
---
+++ spec/mixlib/authorization/models/join_document_spec.rb
@@ -0,0 +1,98 @@
+require File.expand_path('../../../../spec_helper', __FILE__)
+
+
+
+
+describe Mixlib::Authorization::Models::JoinDocument do
+  def actor
+    Mixlib::Authorization::Models::JoinTypes::Actor
+  end
+
+  before do
+    # Calling this a "join document" is confusing because that usually means
+    # metadata associated with the join itself. This class is more like a model
+    # backed by web service resources
+
+    @authorization_service_uri = "http://localhost:5959"
+  end
+
+  describe "when the actor does not exist" do
+    before do
+      @authz_model = actor.new(@authorization_service_uri, "requester_id" => 0, "object_id" => "f" * 32)
+    end
+
+    it "raises NotFound when loading the authz data" do
+      lambda { @authz_model.fetch}.should raise_error(RestClient::ResourceNotFound)
+    end
+
+    it "crashes authz when loading the ACL" do
+      # It would be nice if this was a 404 instead (at minimum).
+      lambda { @authz_model.fetch_acl }.should raise_error(RestClient::InternalServerError)
+    end
+
+  end
+
+  describe "when an actor has been created" do
+    before do
+      @created_authz_model = actor.new(@authorization_service_uri, "requester_id" => 0)
+      @created_authz_model.save
+      @authz_model = actor.new(@authorization_service_uri, "requester_id" => 0, "object_id" => @created_authz_model.id)
+    end
+
+    it "loads the authz object" do
+      # The authz object doesn't actually have interesting data. but whatever
+      @authz_model.fetch.should == {"id" => @authz_model.id}
+    end
+
+    it "loads the object's ACL" do
+      acl_data = @authz_model.fetch_acl
+
+      acl_data["create"].should == {"actors"=>[@authz_model.id, "0"], "groups"=>[]}
+      acl_data["read"].should   == {"actors"=>[@authz_model.id, "0"], "groups"=>[]}
+      acl_data["update"].should == {"actors"=>[@authz_model.id, "0"], "groups"=>[]}
+      acl_data["delete"].should == {"actors"=>[@authz_model.id, "0"], "groups"=>[]}
+      acl_data["grant"].should  == {"actors"=>[@authz_model.id, "0"], "groups"=>[]}
+    end
+
+    it "authorizes its creator for all operations" do
+      %w{create read update delete grant}.each do |ace_type|
+        @authz_model.should be_authorized("0", ace_type)
+      end
+    end
+
+    it "begrudgingly supports checking permissions via #is_authorized?" do
+      %w{create read update delete grant}.each do |ace_type|
+        @authz_model.is_authorized?("0", ace_type).should be_true
+      end
+    end
+
+    it "adds an actor to an ACE" do
+      @authz_model.grant_permission_to_actor("read", "123abc")
+      @authz_model.fetch_acl["read"]["actors"].should include("123abc")
+    end
+
+    describe "when applying ACLs from a parent object" do
+      before do
+        @container = Mixlib::Authorization::Models::JoinTypes::Object.new(@authorization_service_uri, "requester_id" => 0)
+        @container.save
+        %w{create read update delete grant}.each do |ace_type|
+          @container.grant_permission_to_actor(ace_type, "fff000")
+        end
+        @authz_model.apply_parent_acl(@container)
+      end
+
+      it "has inherited permissions from the parent" do
+        acl_data = @authz_model.fetch_acl
+
+        acl_data["create"].should == {"actors"=>[@authz_model.id, "0", "fff000"], "groups"=>[]}
+        acl_data["read"].should   == {"actors"=>[@authz_model.id, "0", "fff000"], "groups"=>[]}
+        acl_data["update"].should == {"actors"=>[@authz_model.id, "0", "fff000"], "groups"=>[]}
+        acl_data["delete"].should == {"actors"=>[@authz_model.id, "0", "fff000"], "groups"=>[]}
+        acl_data["grant"].should  == {"actors"=>[@authz_model.id, "0", "fff000"], "groups"=>[]}
+      end
+
+    end
+
+  end
+
+end

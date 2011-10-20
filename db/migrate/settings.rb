a13bb8e470316f1e73bf0af169576d6b8a15183e
Seth Falcon <seth@opscode.com>
2011-10-20 13:43:31 -0700

    Update default db settings to give case sensitive collation

--- db/migrate/settings.rb
+++ db/migrate/settings.rb
@@ -1,4 +1,6 @@
 # Set defaults for MySQL engine and such
 Sequel::MySQL.default_engine = 'InnoDB'
 Sequel::MySQL.default_charset = 'utf8'
+# Ensure we have a case sensitive collation on the db
+Sequel::MySQL.default_collate = 'utf8_bin'
 

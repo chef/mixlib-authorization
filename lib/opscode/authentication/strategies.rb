70b76139ee5ee89628e101a3174fe243933deacf
Seth Chisamore <schisamo@opscode.com>
2012-02-08 13:52:59 -0500

    simplify the underlying logic for adding/loading strategies

--- lib/opscode/authentication/strategies.rb
+++ lib/opscode/authentication/strategies.rb
@@ -2,24 +2,13 @@ require 'opscode/authentication/strategies/base'
 require 'opscode/authentication/strategies/ldap'
 require 'opscode/authentication/strategies/local'
 
-
 module Opscode
   module Authentication
     module Strategies
       class << self
+
         # Add a strategy and store it in a hash.
-        def add(label, strategy = nil, &block)
-          strategy ||= Class.new(Opscode::Authentication::Strategies::Base)
-          strategy.class_eval(&block) if block_given?
-
-          unless strategy.method_defined?(:authenticate)
-            raise NoMethodError, "authenticate is not declared in the #{label.inspect} strategy"
-          end
-
-          unless strategy.ancestors.include?(Opscode::Authentication::Strategies::Base)
-            raise "#{label.inspect} is not a #{base}"
-          end
-
+        def add(label, strategy = nil)
           strategies[label] = strategy
         end
 
@@ -32,8 +21,7 @@ module Opscode
           @strategies ||= {}
         end
 
-        # convience method for loading all built-in
-        # strategies
+        # convience method for loading all built-in strategies
         def builtin!(user_mapper=nil, options=nil)
           user_mapper ||= Opscode::Mappers::User.new(Opscode::Mappers.default_connection, nil, 0)
           self.add(:local, Opscode::Authentication::Strategies::Local.new(user_mapper, options))

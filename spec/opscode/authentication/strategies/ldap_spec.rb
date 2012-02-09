d3d827b62857eaa7c020a7451bb8128a58d12483
Seth Chisamore <schisamo@opscode.com>
2012-02-09 13:20:17 -0500

    add specs for configuration related logic in ldap authn strategy

--- spec/opscode/authentication/strategies/ldap_spec.rb
+++ spec/opscode/authentication/strategies/ldap_spec.rb
@@ -4,25 +4,27 @@ describe Opscode::Authentication::Strategies::LDAP do
 
   include_context "authentication strategies"
 
-  let(:user) do
-    OpenStruct.new(:uid => 'foo')
-  end
+  let(:default_base) { "dc=opscode,dc=com" }
+  let(:default_options) { {:base => default_base} }
+  let(:user) { OpenStruct.new(:uid => 'foo') }
 
   let(:connection) do
     c = Net::LDAP.new
-    c.stub!(:bind).with({:method=>:simple, :username=>"opscode", :password=>"pedant"}).and_return(true)
-    c.stub!(:bind).with({:method=>:simple, :username=>"opscode", :password=>"FU"}).and_return(false)
+    c.stub!(:bind).with({:method=>:simple,
+      :username=>"uid=#{login},dc=opscode,dc=com", :password=>"pedant"}).and_return(true)
+    c.stub!(:bind).with({:method=>:simple,
+      :username=>"uid=#{login},dc=opscode,dc=com", :password=>"FU"}).and_return(false)
     c.stub!(:search).with(any_args()).and_yield(user)
     c
   end
 
   let!(:strategy) do
-    s = Opscode::Authentication::Strategies::LDAP.new(user_mapper)
+    s = Opscode::Authentication::Strategies::LDAP.new(user_mapper, default_options)
     s.stub!(:connection).and_return(connection)
     s
   end
 
-  it_behaves_like 'an authentication strategy'
+  it_behaves_like "an authentication strategy"
 
   it "should yield the LDAP entry on successful authentication" do
     strategy.authenticate?(login, password) do |entry|
@@ -30,7 +32,24 @@ describe Opscode::Authentication::Strategies::LDAP do
     end
   end
 
-  context 'upstream LDAP system is unavailable' do
+  context "configuration" do
+    it "should set some sane defaults" do
+      strategy.port.should == 389
+      strategy.uid.should == 'uid'
+      strategy.bind_login_format == "%{uid}=%{login},%{base}"
+    end
+
+    it "should allow customizaiton of the bind login format" do
+      s = Opscode::Authentication::Strategies::LDAP.new(user_mapper,
+            default_options.merge(:bind_login_format => "FAKE-%{uid}-FORMAT-%{login}-YO-%{base}"))
+      s.stub!(:connection).and_return(connection)
+      auth_hash = {:method=>:simple, :username=>"FAKE-uid-FORMAT-#{login}-YO-#{default_base}", :password=>"pedant"}
+      connection.should_receive(:bind).with(auth_hash)
+      s.authenticate(login, password)
+    end
+  end
+
+  context "upstream LDAP system is unavailable" do
     let!(:strategy) do
       s = Opscode::Authentication::Strategies::LDAP.new(user_mapper)
       s

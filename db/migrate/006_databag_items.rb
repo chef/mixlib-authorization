6ee4c9a8a694a3aaf6b755aade9be5a9cef00f7f
Mark Anderson <ot-git@muking.org>
2012-03-06 17:02:38 -0800

    Remove authz_id, and add docs about use of multiple key indices.

--- db/migrate/006_databag_items.rb
+++ db/migrate/006_databag_items.rb
@@ -5,9 +5,8 @@ Sequel.migration do
 
     create_table(:databag_items) do
       String(:id, :primary_key => true, :fixed => true, :size => 32)
-      # This should be null right now, but we want it for future use
-      # when databag_items have their own acls.
-      String(:authz_id, :null => false, :fixed => true, :size => 32, :unique => true)
+      # We deliberately left out authz_id, but we will add it in the future when databag_items 
+      # have their own ACLs
 
       # We considered merging :org_id and :databag_name into a single
       # field using the guid of the databag, but decided against
@@ -30,7 +29,13 @@ Sequel.migration do
         raise "Unsupported database"
       end
 
-      unique([:org_id, :databag_name, :item_name]) # only one databag item with a given databag name / item_name in an org
+      # only one databag item with a given databag name / item_name in
+      # an org mysql and pgsql both allow lookups on indices with
+      # partial prefixes; e.g.  we get the benefit of the index if we
+      # are looking up org_id/databag_name, as we will when we list
+      # all the items in a databag. (see postgres docs section 11.5,
+      # "Combining Multiple Indexes") (mysql apparently does the same thing)
+      unique([:org_id, :databag_name, :item_name]) 
     end
     if defined?(Sequel::Postgres)
       # is there a better way to achieve this in Sequel?

95e144020d5874675aca3beba6abf8be97cc9bf2
Mark Anderson <ot-git@muking.org>
2012-03-07 16:58:38 -0800

    Rename databags to data_bags to keep things consistient.

--- db/migrate/005_data_bags.rb
+++ db/migrate/005_data_bags.rb
@@ -3,7 +3,7 @@ require File.expand_path('../settings', __FILE__)
 Sequel.migration do
   change do
 
-    create_table(:databags) do
+    create_table(:data_bags) do
       String(:id, :primary_key => true, :fixed => true, :size => 32)
       String(:authz_id, :null => false, :fixed => true, :size => 32, :unique => true)
       String(:org_id, :null => false, :index => true, :fixed => true, :size => 32)
@@ -13,7 +13,7 @@ Sequel.migration do
       DateTime(:created_at, :null => false)
       DateTime(:updated_at, :null => false)
 
-      unique([:org_id, :name]) # only one databag with a given name in an org
+      unique([:org_id, :name]) # only one data_bag with a given name in an org
     end
   end
 end
--- db/migrate/006_data_bag_items.rb
+++ db/migrate/006_data_bag_items.rb
@@ -3,18 +3,18 @@ require File.expand_path('../settings', __FILE__)
 Sequel.migration do
   change do
 
-    create_table(:databag_items) do
+    create_table(:data_bag_items) do
       String(:id, :primary_key => true, :fixed => true, :size => 32)
-      # We deliberately left out authz_id, but we will add it in the future when databag_items 
+      # We deliberately left out authz_id, but we will add it in the future when data_bag_items 
       # have their own ACLs
 
-      # We considered merging :org_id and :databag_name into a single
-      # field using the guid of the databag, but decided against
+      # We considered merging :org_id and :data_bag_name into a single
+      # field using the guid of the data_bag, but decided against
       # because that would lock us into making two lookups to find a
-      # databag item.
-      # index is handled with unique index on org/databag_name/item_name combo
+      # data_bag item.
+      # index is handled with unique index on org/data_bag_name/item_name combo
       String(:org_id, :null => false, :index => true, :fixed => true, :size => 32)
-      String(:databag_name, :null => false)
+      String(:data_bag_name, :null => false)
       String(:item_name, :null => false)
 
       String(:last_updated_by, :null => false, :fixed => true, :size => 32)
@@ -29,20 +29,20 @@ Sequel.migration do
         raise "Unsupported database"
       end
 
-      # only one databag item with a given databag name / item_name in
+      # only one data_bag item with a given data_bag name / item_name in
       # an org mysql and pgsql both allow lookups on indices with
       # partial prefixes; e.g.  we get the benefit of the index if we
-      # are looking up org_id/databag_name, as we will when we list
-      # all the items in a databag. (see postgres docs section 11.5,
+      # are looking up org_id/data_bag_name, as we will when we list
+      # all the items in a data_bag. (see postgres docs section 11.5,
       # "Combining Multiple Indexes") (mysql apparently does the same thing)
-      unique([:org_id, :databag_name, :item_name]) 
+      unique([:org_id, :data_bag_name, :item_name]) 
     end
     if defined?(Sequel::Postgres)
       # is there a better way to achieve this in Sequel?
       # For Postgresql, the default storage strategy compresses
       # data. Since we intend to store gzip data here, we set the type
       # to EXTERNAL to avoid compression.
-      run("ALTER TABLE databag_items ALTER serialized_object SET STORAGE EXTERNAL")
+      run("ALTER TABLE data_bag_items ALTER serialized_object SET STORAGE EXTERNAL")
     end
   end
 end

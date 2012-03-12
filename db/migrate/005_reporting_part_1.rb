815149d900fac4a91f6a0b3e117d6e295d12989d
Marc Paradise <marc@opscode.com>
2012-03-12 16:58:07 -0400

    all changes should be reversible - even FKs...
    * original code would fail during sequel migrate -M 0 b/c the foreign
      keys would prevent the drop.  Implemented custom 'down' block to
      ensure that drops occur in correct sequence to prevent any error.

--- db/migrate/005_reporting_part_1.rb
+++ db/migrate/005_reporting_part_1.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../settings', __FILE__)
-
 Sequel.migration do
-  change do
+  up do
     # There is no apparent varbinary support in Sequel.  This
     # means a couple of manual workarounds. 
     #
     run("ALTER TABLE nodes ADD COLUMN last_audit_id VARBINARY(16) DEFAULT NULL")
-
 
     # audit_id should be varbinary - for now we'll declare it as a
     # placeholder of varchar so that we can create the primary key. 
@@ -18,7 +16,6 @@ Sequel.migration do
       DateTime(:end_time, :null => true)
       text(:event_data, :null => true)
       foreign_key([:node_id], :nodes, :name => :node_id_fk)
-
     end
 
     # Manually change the column data type. 
@@ -27,8 +24,8 @@ Sequel.migration do
     # Same drill: audit_id is declared here as a placeholder so we can
     # include it as part of the primary key; we'll change the type
     # afterwards. 
-    create_table(:node_audit_detail) do 
-      String(:audit_id, :fixed => false, :size => 16) 
+    create_table(:node_audit_detail) do
+      String(:audit_id, :fixed => false, :size => 16)
       int(:seq, :null => false)
       int(:duration, :null => false)
       String(:res_id, :fixed => false,  :null => false, :size => 255)
@@ -42,10 +39,28 @@ Sequel.migration do
     end
 
     run("ALTER TABLE node_audit_detail MODIFY audit_id VARBINARY(16) NOT NULL"); 
-    
+
     # We couldn't add FK before column data types matched.
     alter_table(:node_audit_detail) do 
       add_foreign_key([:audit_id], :node_audit, :name => :audit_id_fk);
     end
   end
+
+  # because of hte foreign keys we need to deconstruct our tables in a
+  # specific order. 
+  #
+  down do
+    alter_table(:nodes) do 
+      drop_column :last_audit_id;
+    end
+
+    drop_table(:node_audit_detail)
+
+    drop_table(:node_audit)
+
+  end
 end
+# Sequel.migration do
+#  change do
+#  end
+# end

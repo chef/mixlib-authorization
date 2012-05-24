5048a4de40d783fa4dc3091e6c190f613c887b86
Seth Chisamore <schisamo@opscode.com>
2012-05-24 10:15:12 -0400

    move node status to it's own table

--- db/migrate/012_node_status.rb
+++ db/migrate/012_node_status.rb
@@ -2,8 +2,15 @@ require File.expand_path('../settings', __FILE__)
 
 Sequel.migration do
   change do
-    alter_table(:nodes) do
-      add_column :status, Integer, :null => true
+
+    create_table(:node_statuses) do
+      String(:id, :primary_key => true, :fixed => true, :size => 32)
+      String(:node_name, :null => false, :unique => true)
+      Integer(:status, :null => false)
+
+      # String(:last_updated_by, :null => false, :fixed => true, :size => 32)
+      # DateTime(:created_at, :null => false)
+      # DateTime(:updated_at, :null => false)
     end
   end
 end

45e1dfa2f81f3f0bc3bc4b9e10dc6edaab76786f
Seth Chisamore <schisamo@opscode.com>
2012-05-25 18:14:16 -0400

    node_statuses => node_status; remove UUID primary key

--- db/migrate/012_node_status.rb
+++ db/migrate/012_node_status.rb
@@ -3,8 +3,7 @@ require File.expand_path('../settings', __FILE__)
 Sequel.migration do
   change do
 
-    create_table(:node_statuses) do
-      String(:id, :primary_key => true, :fixed => true, :size => 32)
+    create_table(:node_status) do
       String(:org_id, :null => false, :fixed => true, :size => 32)
       String(:node_name, :null => false)
       Integer(:status, :null => false)
@@ -12,6 +11,7 @@ Sequel.migration do
       String(:last_updated_by, :null => false, :fixed => true, :size => 32)
       DateTime(:created_at, :null => false)
       DateTime(:updated_at, :null => false)
+      primary_key([:org_id, :node_name])
       unique([:org_id, :node_name]) # only one node with a given name in an org
     end
   end

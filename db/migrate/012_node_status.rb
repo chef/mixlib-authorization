d78e98f753f0ebd5e7ac90be55726ac5fc662a51
Seth Chisamore <schisamo@opscode.com>
2012-05-25 17:04:08 -0400

    add org_id and our record keeping columns to node_statuses

--- db/migrate/012_node_status.rb
+++ db/migrate/012_node_status.rb
@@ -5,12 +5,14 @@ Sequel.migration do
 
     create_table(:node_statuses) do
       String(:id, :primary_key => true, :fixed => true, :size => 32)
-      String(:node_name, :null => false, :unique => true)
+      String(:org_id, :null => false, :fixed => true, :size => 32)
+      String(:node_name, :null => false)
       Integer(:status, :null => false)
 
-      # String(:last_updated_by, :null => false, :fixed => true, :size => 32)
-      # DateTime(:created_at, :null => false)
-      # DateTime(:updated_at, :null => false)
+      String(:last_updated_by, :null => false, :fixed => true, :size => 32)
+      DateTime(:created_at, :null => false)
+      DateTime(:updated_at, :null => false)
+      unique([:org_id, :node_name]) # only one node with a given name in an org
     end
   end
 end

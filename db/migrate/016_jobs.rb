04e35fe8993988ce2099361c162a0cb9fa392d22
Seth Chisamore <schisamo@opscode.com>
2012-05-31 18:59:41 -0400

    initial schema for `jobs` and `job_nodes` tables

---
+++ db/migrate/016_jobs.rb
@@ -0,0 +1,31 @@
+require File.expand_path('../settings', __FILE__)
+
+Sequel.migration do
+  change do
+
+    # For Pushy PoC simplicity:
+    # * we are using node_name (vs node_id) with no foreign key constraint
+    # * authz_id column has intentionally been left out for PoC simplicity
+
+    create_table(:jobs) do
+      primary_key :id # autoincrementing integer
+      String(:org_id, :null => false, :fixed => true, :size => 32)
+      Integer(:status, :null => false)
+
+      String(:last_updated_by, :null => false, :fixed => true, :size => 32)
+      DateTime(:created_at, :null => false)
+      DateTime(:updated_at, :null => false)
+    end
+
+    create_table(:job_nodes) do
+      String(:node_name, :null => false)
+      # String(:node_id, :null => :false, :fixed => true, :size => 32)
+      String(:org_id, :null => false, :fixed => true, :size => 32)
+      Integer(:status, :null => false)
+
+      foreign_key(:job_id, :jobs, :on_delete => :restrict)
+      #foreign_key([:org_id, :node_id], :nodes, :key => [:org_id, :node_id], :on_delete => :cascade, :on_update => :cascade)
+    end
+
+  end
+end

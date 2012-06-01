dd06ce6ebc210597553314db66073c35f9012fc7
Seth Chisamore <schisamo@opscode.com>
2012-06-01 18:09:16 -0400

    Change jobs table's id column to a genrated GUID
    Should make job_nodes record insert logic much easier
    for the PoC.  Right now SQERL doesn't return generated
    primary key IDs on inserts.

--- db/migrate/016_jobs.rb
+++ db/migrate/016_jobs.rb
@@ -8,7 +8,7 @@ Sequel.migration do
     # * authz_id column has intentionally been left out for PoC simplicity
 
     create_table(:jobs) do
-      primary_key :id # autoincrementing integer
+      String(:id, :primary_key => true, :fixed => true, :size => 32)
       String(:org_id, :null => false, :fixed => true, :size => 32)
       Integer(:status, :null => false)
 
@@ -18,6 +18,7 @@ Sequel.migration do
     end
 
     create_table(:job_nodes) do
+      String(:job_id, :null => :false, :fixed => true, :size => 32)
       String(:org_id, :null => false, :fixed => true, :size => 32)
       String(:node_name, :null => false)
       # String(:node_id, :null => :false, :fixed => true, :size => 32)
@@ -26,8 +27,8 @@ Sequel.migration do
       DateTime(:created_at, :null => false)
       DateTime(:updated_at, :null => false)
 
-      unique([:org_id, :node_name, :job_id]) # only one node with a given name/org per job
-      foreign_key(:job_id, :jobs, :on_delete => :restrict)
+      unique([:job_id, :org_id, :node_name]) # only one node with a given name/org per job
+      foreign_key([:job_id], :jobs, :key => :id, :on_delete => :cascade, :on_update => :cascade)
       #foreign_key([:org_id, :node_id], :nodes, :key => [:org_id, :node_id], :on_delete => :cascade, :on_update => :cascade)
     end
 

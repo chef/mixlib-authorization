8eb6fa1b64b4f9716be42e1bad15baccd20cca3a
Ho-Sheng Hsiao <hosh@opscode.com>
2012-07-16 10:58:08 -0400

    [OC-1975] Removed migration and schema definitions

--- db/migrate/009_data_bag_item_fk.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../settings', __FILE__)
-
-Sequel.migration do
-  change do
-    alter_table(:data_bag_items) do
-      add_foreign_key [:org_id, :data_bag_name], :data_bags, :key => [:org_id, :name], :on_delete => :cascade, :on_update => :cascade
-    end
-  end
-end
